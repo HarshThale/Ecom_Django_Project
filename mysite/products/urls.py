69445c0d4f571e9804e57376ada797c68262f696
HarshThale <thaleharsh48@gmail.com>
2023-11-26 11:21:35 +0530

    commit:20

--- mysite/products/urls.py
+++ mysite/products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from products import views
 
+app_name = 'products'
+
 urlpatterns = [
     path('home/', views.index, name='index'),
     path('detail/<int:item_id>/', views.detail, name='detail'),
