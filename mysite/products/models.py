8ec5f3194f2456a4b535368793da9e0d0574821f
HarshThale <thaleharsh48@gmail.com>
2023-11-26 15:31:38 +0530

    commit:26

--- mysite/products/models.py
+++ mysite/products/models.py
@@ -6,6 +6,10 @@ class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_desc = models.CharField(max_length=300)
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500,
+        default="https://assets.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e3a57bdb3717fbf9cec_Product_Default.svg"
+    )
     
     def __str__(self):
         return self.item_name
