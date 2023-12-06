94a41c34595f3c812eab2842edef2655592ed0c9
HarshThale <thaleharsh48@gmail.com>
2023-12-06 11:20:06 +0530

    commit:60

--- mysite/products/views.py
+++ mysite/products/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from products.models import Item
 from products.forms import ItemForm
+from django.views.generic.list import ListView
 
 # Create your views here.
+# -------------------------------------------------------------------------------
+
+
+# function based index view
+# -------------------------------------------------------------------------------
 
 def index(request):
     itemlist = Item.objects.all()
@@ -14,6 +20,20 @@ def index(request):
     
     return render(request, 'products/index.html', context)
 
+
+# class based index view
+# -------------------------------------------------------------------------------
+
+class IndexClassView(ListView):
+
+    model = Item
+    context_object_name = 'itemlist'
+    template_name = 'products/index.html'
+
+
+# function based detail view
+# -------------------------------------------------------------------------------
+
 def detail(request, item_id):
     item = Item.objects.get(pk=item_id)
 
@@ -22,6 +42,10 @@ def detail(request, item_id):
     }
     
     return render(request, 'products/detail.html', context)
+
+
+# function based create item view
+# -------------------------------------------------------------------------------
 
 def create_item(request):
     form = ItemForm(request.POST or None)
@@ -36,6 +60,10 @@ def create_item(request):
 
     return render(request, 'products/item-form.html', context)
 
+
+# function based update item view
+# -------------------------------------------------------------------------------
+
 def update_item(request, id):
     item = Item.objects.get(pk=id)
     form = ItemForm(request.POST or None, instance=item)
@@ -49,6 +77,10 @@ def update_item(request, id):
         return redirect('products:index')
     
     return render(request, 'products/item-form.html', context)
+
+
+# function based delete item view
+# -------------------------------------------------------------------------------
 
 def delete_item(request, id):
     item = Item.objects.get(pk=id)
